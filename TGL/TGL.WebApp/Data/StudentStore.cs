62e641f2cad03c54f6be9792c15123d6ee3444f4
Daniel Londoño Ospina <dani3l.od@msn.com>
2019-10-26 12:38:35 -0500

    Conexión con Base de datos

--- TGL/TGL.WebApp/Data/StudentStore.cs
+++ TGL/TGL.WebApp/Data/StudentStore.cs
@@ -8,44 +8,11 @@ namespace TGL.WebApp.Data
 {
     public class StudentStore
     {
-        private List<Student> Students { get; set; } = new List<Student>(); //Inicializa Lista
-        public StudentStore()
+        //private List<Student> Students { get; set; } = new List<Student>(); //Inicializa Lista
+        public TGLContext Context { get; set; }
+        public StudentStore(TGLContext context)
         {
-            Students.Add(new Student
-            {
-                Age=26,
-                Name="Daniel",
-                LastName="Londoño",
-                Nit="1037629046"
-            });
-            Students.Add(new Student
-            {
-                Age = 15,
-                Name = "El",
-                LastName = "Bromas",
-                Nit = "100"
-            });
-            Students.Add(new Student
-            {
-                Age = 30,
-                Name = "Tribi",
-                LastName = "Lin",
-                Nit = "654"
-            });
-            Students.Add(new Student
-            {
-                Age = 17,
-                Name = "Bad",
-                LastName = "Bunny",
-                Nit = "15234"
-            });
-            Students.Add(new Student
-            {
-                Age = 31,
-                Name = "Esteban",
-                LastName = "Quito",
-                Nit = "1557864"
-            });
+            Context = context;
         }
 
         internal void EditStudent(Student student)
@@ -55,30 +22,31 @@ namespace TGL.WebApp.Data
             currentStudent.LastName = student.LastName;
             currentStudent.Age = student.Age;
             currentStudent.Nit = student.Nit;
+            Context.Student.Update(currentStudent);
+            Context.SaveChanges();
         }
 
         internal Student GetStudentById(Guid id)
         {
-            return Students.FirstOrDefault(x => x.Id == id);
+            return Context.Student.FirstOrDefault(x => x.Id == id);
         }
 
         internal void AddStudent(Student student)
         {
-            Students.Add(student);
+            Context.Student.Add(student);
+            Context.SaveChanges();
 
         }
 
         internal void DeleteStudent(Guid id)
         {
-            var student = Students.FirstOrDefault(x => x.Id == id);
-            Students.Remove(student);
+            var student = Context.Student.FirstOrDefault(x => x.Id == id);
+            Context.Student.Remove(student);
+            Context.SaveChanges();
         }
 
         public List<Student> GetStudents() {
-            return this.Students;
+            return Context.Student.ToList();
         }
-
-
-
     }
 }
